53ed0ee4ec11d5f7487b01496030862b4e8d5e3f
jueves <martes1+varios@fastmail.fm>
2023-03-24 16:06:10 +0000

    Compose podcasts.rss file

--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
-This is a podcast RSS generator.
-Given an audio file and its metadata, it adds it to the current RSS file.
+This is a podcast RSS generator.  
+Given an audio file and its metadata, it adds it to the current RSS file.  
 If given an URL it downloads and extracts the audio and metadata and adds it to the RSS file.
+
+### To do
+- [X] Compose RSS file.
+- [ ] Replace argparser arguments with standard input.
+- [ ] Add yt-dlp support.
+
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import json
 import os
 
 json_file = "podcasts.json"
+header_file = "header.rss"
+footer_file = "footer.rss"
+rss_file = "podcasts.rss"
 server_ip = "10.69.0.3"
 
 parser = argparse.ArgumentParser(
@@ -11,7 +14,7 @@ parser = argparse.ArgumentParser(
     description="Adds a new podcast entry in the RSS for a given audio file."
     )
 parser.add_argument("filename", help="Audio file name")
-parser.add_argument("-t", "--tittle", help = "Podcast chapter title", default=parser.parse_args().filename)
+parser.add_argument("-t", "--title", help = "Podcast chapter title", default="none")
 parser.add_argument("-l", "--link", help = "Podcast chapter URL (not audio URL)", default="none")
 parser.add_argument("-d", "--description", help = "Podcast chapter description", default="none")
 parser.add_argument("--audiourl", default = "none")
@@ -22,16 +25,16 @@ parser.add_argument("--pubDate", help = "Date in which the podcast chapter was r
 
 args = parser.parse_args()
 
-with open(json_file) as f:
-    podcasts_dic = json.load(f)
+# Set default title
+if (args.title == "none"):
+    args.title = args.filename
 
 # Set proper audio url
 if args.filename[:4] == "http":
     args.audiourl = args.filename
 else:
-    os.rename(args.filename, "podcasts/" + args.filename)
+    os.rename(args.filename, "html/podcasts/" + args.filename)
     args.audiourl = "http://" + server_ip + "/podcasts/" + args.filename
-
 
 # Define label strings
 title_label = '            {\n               "title": '
@@ -42,4 +45,62 @@ audio_type_label = ',\n                  "_type": '
 pubDate_label = '\n               },\n               "pubDate": '
 end_podcast_label = '\n            },\n'
 
+# Load current podcasts
+with open(json_file) as f:
+    podcasts_list = json.load(f)
+
+# Add new podcast
+new_podcast = {
+    'title': args.title,
+    'link': args.link,
+    'description': args.description,
+    'audio_url': args.audiourl,
+    'audio_type': args.audiotype,
+    'pubDate': args.pubDate
+}
+
+podcasts_list.append(new_podcast)
+
+
+# Load header and footer
+with open(header_file) as f:
+    header = f.read()
+
+with open(footer_file) as f:
+    footer = f.read()
+
 # Create RSS file
+## Create file with header
+rss = header
+
+## Iterate throught podcasts
+for podcast in podcasts_list:
+    rss = "".join([rss, title_label + podcast['title'],
+             link_label + podcast['link'],
+             description_label, podcast['description'],
+             audio_url_label, podcast['audio_url'],
+             audio_type_label, podcast['audio_type'],
+             pubDate_label, podcast['pubDate'],
+             end_podcast_label
+             ])
+    
+'''
+    rss = rss + title_label + podcast['title'],
+    rss = rss + link_label + podcast['link'],
+    rss = rss + description_label + podcast['description']
+    rss = rss + audio_url_label + podcast['audio_url']
+    rss = rss + audio_type_label + podcast['audio_type']
+    rss = rss + pubDate_label + podcast['pubDate']
+    print("Fin de bucle")
+
+'''
+    
+## Add footer
+rss = rss + footer
+
+print("Lass rss type:")
+print(type(rss))
+
+## Write file
+with open(rss_file, "w") as f:
+    f.write(rss)
